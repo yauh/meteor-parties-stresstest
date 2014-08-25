918a26b64d500e5b7a23c20634fb0447d15d425f
Stephan Hochhaus <stephan@yauh.de>
2014-08-25 12:54:47 +0200

    easily start multiple local instances

--- start-local.sh
+++ start-local.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
+# read options
+while getopts ":i:" opt; do
+  case $opt in
+    i)
+      echo "Will run $OPTARG times" >&2
+      drones="$OPTARG"
+      ;;
+      \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
-
+i=0
 # run casper locally
-nohup ./immortalCasper.sh -u http://localhost:3000 -p password > /dev/null 2>&1 &
+while [[ $i -lt $drones ]]
+do
+    nohup ./immortalCasper.sh -u http://localhost:3000 -p password > /dev/null 2>&1 &
+    i=$[$i+1]
+done
